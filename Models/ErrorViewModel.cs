82bb6bc76c65337cb18c90f8f55c8f2cc5461db6
LeThuTrang <thutrang0612.ilu@gmail.com>
2023-10-08 14:39:23 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LTT_PTPMQL_BKT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
